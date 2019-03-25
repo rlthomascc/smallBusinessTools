4f3e3710d773a1326bb4ece8626d0989605c5118
Randy <rlthomasccv@gmail.com>
2019-03-24 17:28:45 -0700

    Finalize layout to Agent page.

--- client/src/components/AgentPage.js
+++ client/src/components/AgentPage.js
@@ -3,7 +3,8 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-useless-constructor */
 import React, { Component } from 'react';
-import AgentChart from '../charts/AgentChart';
+// import { timingSafeEqual } from 'crypto';
+// import AgentChart from '../charts/AgentChart';
 
 
 class AgentPage extends Component {
@@ -12,7 +13,6 @@ class AgentPage extends Component {
   }
 
   agentTop() {
-    console.log(this.props, 'AGENT');
     const { agents, transactions } = this.props;
     const getImage = () => (agents.length > 0 ? agents[0].image.substring(11) : []);
     const getName = () => (agents.length > 0 ? agents[0].name : []);
@@ -29,9 +29,8 @@ class AgentPage extends Component {
         return `$${ytd.toLocaleString()}`;
       }
     };
-    console.log(getYTD(), 'ytd');
     return (
-      <div id="homePage">
+      <div>
         <div className="d-flex flex-row">
           <div className="p-2">
             <img alt="portrait" src={getImage()} width="150" className="rounded-circle" />
@@ -55,23 +54,78 @@ class AgentPage extends Component {
             </p>
           </div>
         </div>
+        <br />
+        <div className="d-flex flex-row">
+          <p>Brief description about the person goes here: </p>
+        </div>
+      </div>
+    );
+  }
+
+  agentTable() {
+    return (
+      <div className="container col-sm-12">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Address</th>
+              <th scope="col">Total Price</th>
+              <th scope="col">To Self</th>
+              <th scope="col">To DRG</th>
+              <th scope="col">Side</th>
+              <th scope="col">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>2099 Brennan Ln.</td>
+              <td>$250,000</td>
+              <td>$4000</td>
+              <td>$4000</td>
+              <td>Buyer</td>
+              <td>Sun Mar 24 2019</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  totalAgent() {
+    const { agents, transactions } = this.props;
+    const getTransactions = () => (agents.length > 0 ? agents[0].transactions : []);
+    const getYTD = () => {
+      let ytd = 0;
+      if (transactions.length > 0) {
+        transactions.map((trans, i) => {
+          if (trans.agent === 'Carlos') {
+            ytd += trans.commission;
+          }
+        });
+      }
+    };
+    return (
+      <div className="bg-light border border-default alert alert-light col-sm-3">
+        <h1>TEST</h1>
       </div>
     );
   }
 
   agent() {
-    const { chartData } = this.state;
     return (
-      <div className="agentPage" id="homePage">
-        <AgentChart chartData={chartData} />
-        {/* {this.agentTop} */}
+      <div id="homePage" className="agentPage">
+        {this.agentTop()}
+        {this.agentTable()}
+        {this.totalAgent()}
       </div>
     );
   }
 
   render() {
     return (
-      this.agentTop()
+      this.agent()
     );
   }
 }
